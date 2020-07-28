1d42ab893354c5ab3915948935e50a41ca4d0c34
WuTim0307 <upk.rd00003@gmail.com>
2020-07-28 18:52:12 +0800

    feat: 現行可運行版本

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,11 +13,26 @@ import pricedown from './pricedown/pricedown';
 
 
 const Layout = () => {
-    // const {page, setPage} = useContext(CurrentPage);
     return (
         <Router>
+            <div>
+                <ul>
+                    <li><Link to="/">首頁</Link></li>
+                    <li><Link to="/limitedtime">限時</Link></li>
+                    <li><Link to="/newgood">新品</Link></li>
+                    <li><Link to="/onlineonly">線上獨家</Link></li>
+                    <li><Link to="/pricedown">特價</Link></li>
+                </ul>
+                <hr/>
+                <Route path="/" component={home}/>
+                <Route path="/limitedtime" component={limitedtime}/>
+                <Route path="/newgood" component={newgood}/>
+                <Route path="/onlineonly" component={onlineonly}/>
+                <Route path="/pricedown" component={pricedown}/>
+            </div>
+        {/*
             <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="home" to="home">標題ICON</Navbar.Brand>
+                <Navbar.Brand to="home">標題ICON</Navbar.Brand>
                 <Nav className="mr-auto">
                     <Nav.Link href="home" to="home">Home</Nav.Link>
                     <Nav.Link href="pricedown" to="pricedown">特價</Nav.Link>
@@ -31,12 +46,13 @@ const Layout = () => {
                 </Form>
             </Navbar>
             <Switch>
-                <Route exact path="/home" component={home}></Route>
+                <Route path="/home" component={home}></Route>
                 <Route path="/pricedown" component={pricedown}></Route>
                 <Route path="/limitedtime" component={limitedtime}></Route>
                 <Route path="/onlineonly" component={onlineonly}></Route>
                 <Route path="/newgood" component={newgood}></Route>
             </Switch>
+        */}
         </Router>
     );
 }
