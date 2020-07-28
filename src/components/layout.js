b03c4f404b399b3211feb26f9b1d353b94b625a2
WuTim0307 <upk.rd00003@gmail.com>
2020-07-28 17:17:07 +0800

    feat: 多頁練習

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,27 +1,41 @@
-import React, {useState} from "react";
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import React, {useContext} from "react";
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
+import CurrentPage from "../page-content"
+
 
 const Layout = () => {
-    const [search, setSearch] = useState('');
+
     return (
-        <Navbar bg="dark" variant="dark">
-            <Navbar.Brand href="#home">標題ICON</Navbar.Brand>
-            <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#features">特價</Nav.Link>
-                <Nav.Link href="#pricing">限時特價</Nav.Link>
-                <Nav.Link href="#pricing">App會員/網路限定</Nav.Link>
-                <Nav.Link href="#pricing">新品</Nav.Link>
-            </Nav>
-            <Form inline>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2"/>
-                <Button variant="outline-info">Search</Button>
-            </Form>
-        </Navbar>
+        <Router>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand to="home" onClick={() => setPage('home')}>標題ICON</Navbar.Brand>
+                <Switch>
+                    <Route path="/home"></Route>
+                    <Route path="/pricedown"></Route>
+                    <Route path="/limitedtime"></Route>
+                    <Route path="/onlineonly"></Route>
+                    <Route path="/newgood"></Route>
+                </Switch>
+                <Nav className="mr-auto">
+                    <Nav.Link href="home" to="home" onClick={() => setPage('home')}>Home</Nav.Link>
+                    <Nav.Link href="pricedown" to="pricedown" onClick={() => setPage('pricedown')}>特價</Nav.Link>
+                    <Nav.Link href="limitedtime" to="limitedtime"
+                              onClick={() => setPage('limitedtime')}>限時特價</Nav.Link>
+                    <Nav.Link href="onlineonly" to="onlineonly"
+                              onClick={() => setPage('onlineonly')}>App會員/網路限定</Nav.Link>
+                    <Nav.Link href="newgoodZ" to="newgood" onClick={() => setPage('newgood')}>新品</Nav.Link>
+                </Nav>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2"/>
+                    <Button variant="outline-info">Search</Button>
+                </Form>
+            </Navbar>
+        </Router>
     );
 }
 export default Layout;
